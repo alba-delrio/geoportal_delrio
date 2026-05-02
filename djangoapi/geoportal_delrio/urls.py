af234f6e887609d39fec399bb50f90e20aa5c344
alba-delrio <albaddelrio@gmail.com>
2026-05-02 23:21:40 +0200

    Otro cambio en urls

--- djangoapi/geoportal_delrio/urls.py
+++ djangoapi/geoportal_delrio/urls.py
@@ -24,21 +24,62 @@ EN REALIDAD EN POST LOS DATOS VAN POR DETRÁS (DESPUÉS DE ///) PERO PARA SABER 
 '''
 urlpatterns = [
     # --- BARRIOS ---
-    path('barrios/', views.BarriosView.as_view(), name='B_GET_selectall()_POST_insert()'), 
-    path('barrios/<int:id>/', views.BarriosView.as_view(), name='B_GET_selectone(id)_POST_update(id)'),
-    path('barrios/<int:id>/delete/', views.BarriosView.as_view(), name='B_POST_delete(id)'),
+    path('barrios/', 
+         views.BarriosView.as_view(actions={'get': 'selectall', 'post': 'insert'}), 
+         name='B_GET_selectall()_POST_insert()'), 
+    
+    path('barrios/<int:id>/', 
+         views.BarriosView.as_view(actions={'get': 'selectone', 'post': 'update'}), 
+         name='B_GET_selectone(id)_POST_update(id)'),
+    
+    path('barrios/<int:id>/delete/', 
+         views.BarriosView.as_view(actions={'post': 'delete'}), 
+         name='B_POST_delete(id)'),
 
     # --- CLIENTES ---
-    path('clientes/', views.ClientesView.as_view(), name='C_GET_selectall()_POST_insert()'),
-    path('clientes/<int:id>/', views.ClientesView.as_view(), name='C_GET_selectone(id)_POST_update(id)'),
-    path('clientes/<int:id>/delete/', views.ClientesView.as_view(), name='C_POST_delete(id)'),
+    path('clientes/', 
+         views.ClientesView.as_view(actions={'get': 'selectall', 'post': 'insert'}), 
+         name='C_GET_selectall()_POST_insert()'),
+    
+    path('clientes/<int:id>/', 
+         views.ClientesView.as_view(actions={'get': 'selectone', 'post': 'update'}), 
+         name='C_GET_selectone(id)_POST_update(id)'),
+    
+    path('clientes/<int:id>/delete/', 
+         views.ClientesView.as_view(actions={'post': 'delete'}), 
+         name='C_POST_delete(id)'),
 
     # --- RUTAS ---
-    path('rutas/', views.RutasView.as_view(), name='R_GET_selectall()_POST_insert()'),
-    path('rutas/<int:id>/', views.RutasView.as_view(), name='R_GET_selectone(id)_POST_update(id)'),
-    path('rutas/<int:id>/delete/', views.RutasView.as_view(), name='R_POST_delete(id)'),
+    path('rutas/', 
+         views.RutasView.as_view(actions={'get': 'selectall', 'post': 'insert'}), 
+         name='R_GET_selectall()_POST_insert()'),
+    
+    path('rutas/<int:id>/', 
+         views.RutasView.as_view(actions={'get': 'selectone', 'post': 'update'}), 
+         name='R_GET_selectone(id)_POST_update(id)'),
+    
+    path('rutas/<int:id>/delete/', 
+         views.RutasView.as_view(actions={'post': 'delete'}), 
+         name='R_POST_delete(id)'),
 ]
 
+
+# urlpatterns = [
+#     # --- BARRIOS ---
+#     path('barrios/', views.BarriosView.as_view(), name='B_GET_selectall()_POST_insert()'), 
+#     path('barrios/<int:id>/', views.BarriosView.as_view(), name='B_GET_selectone(id)_POST_update(id)'),
+#     path('barrios/<int:id>/delete/', views.BarriosView.as_view(), name='B_POST_delete(id)'),
+
+#     # --- CLIENTES ---
+#     path('clientes/', views.ClientesView.as_view(), name='C_GET_selectall()_POST_insert()'),
+#     path('clientes/<int:id>/', views.ClientesView.as_view(), name='C_GET_selectone(id)_POST_update(id)'),
+#     path('clientes/<int:id>/delete/', views.ClientesView.as_view(), name='C_POST_delete(id)'),
+
+#     # --- RUTAS ---
+#     path('rutas/', views.RutasView.as_view(), name='R_GET_selectall()_POST_insert()'),
+#     path('rutas/<int:id>/', views.RutasView.as_view(), name='R_GET_selectone(id)_POST_update(id)'),
+#     path('rutas/<int:id>/delete/', views.RutasView.as_view(), name='R_POST_delete(id)'),
+# ]
 
 
 
