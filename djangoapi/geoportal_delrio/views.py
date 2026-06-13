1d7a1f600c554f3379ac91deaf2cab71bde22782
alba-delrio <albaddelrio@gmail.com>
2026-06-13 20:01:42 +0200

    Feat: Vistas y urls de autenticacion de usuarios en Django

--- djangoapi/geoportal_delrio/views.py
+++ djangoapi/geoportal_delrio/views.py
@@ -145,7 +145,7 @@ class LoginView(View):
             login(request,user)#introduce into the request cookies the session_id, and in the auth_sessions the session data.This way,in followoing requests, know who is the user and if
     # he is already authenticated.
     # The coockies are sent in the response header on POST requests
-            return JsonResponse({"ok":True,"message": "User {0} logged in".format(username), "data":[{"userame":username}]})
+            return JsonResponse({"ok":True,"message": "User {0} logged in".format(username), "data":[{"username":username}]})
         else:
     # To make thinks difficult to hackers, you make a random delay,# between 0 and 1 second
             seconds=random.uniform(0, 1)
